3eb4907e1d670f0426e02c184204ca7f99d2a8f2
M1036083 <Priyanka.Das@mindtree.com>
2018-03-09 13:23:56 +0530

    Update Startup.cs

--- MVCAppHelloWorld/HelloWorld/HelloWorld/Startup.cs
+++ MVCAppHelloWorld/HelloWorld/HelloWorld/Startup.cs
@@ -27,7 +27,7 @@ namespace HelloWorld
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello, Universe!");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
